e716f68ba272d70b7bdc96fb8e6864e36407b75a
Vasyl Nikolov <vnnikolov1991@gmail.com>
2024-07-28 21:14:17 +0200

    createhw

---
+++ js/task-1.js
@@ -0,0 +1,36 @@
+function isEnoughCapacity(products, containerSize) {
+    let total = 0
+    for (const key in products) {
+        const element = products[key]; 
+              total += element
+        }
+  
+        if (total <= containerSize) {
+            return true
+            
+    }
+        return false
+
+    }
+
+
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
+
+
+
+
